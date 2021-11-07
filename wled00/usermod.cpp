712e53ca8d1f88c3f2dc6c4b452c2116744cab93
Florian Heilmann <Florian.Heilmann@gmx.net>
2021-11-08 00:48:01 +0100

    First implementation of AudioSource microphone code

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -14,142 +14,41 @@
  * Not 100% sure this was done right. There is probably a better way to handle this...
  */
 
-#ifdef USE_ES7243
-// See https://github.com/espressif/esp-adf/blob/master/components/audio_hal/driver/es7243/es7243.c
-
-#include <Wire.h>
-
-#ifndef ES7243_ADDR
-#define ES7243_ADDR 0x13
-#endif
-
-#ifndef ES7243_SDAPIN
-#define ES7243_SDAPIN 18
-#endif
-
-#ifndef ES7243_SCLPIN
-#define ES7243_SCLPIN 23
-#endif
-
-void es7243_i2c_begin()
-{
-  Wire.begin(ES7243_SDAPIN, ES7243_SCLPIN, 100000U);
-}
-void es7243_i2c_write(uint8_t reg, uint8_t val)
-{
-  Wire.beginTransmission(ES7243_ADDR);
-  Wire.write((uint8_t)reg);
-  Wire.write((uint8_t)val);
-  Wire.endTransmission();
-}
-void es7243_i2c_init_adc()
-{
-    es7243_i2c_begin();
-    es7243_i2c_write(0x00, 0x01);
-    es7243_i2c_write(0x06, 0x00);
-    es7243_i2c_write(0x05, 0x1B);
-    es7243_i2c_write(0x01, 0x0C);
-    es7243_i2c_write(0x08, 0x43);
-    es7243_i2c_write(0x05, 0x13);
-}
-
-#endif // USE_ES7243
-
 // This gets called once at boot. Do all initialization that doesn't depend on network here
 void userSetup() {
+  // Reset I2S peripheral for good measure
+  i2s_driver_uninstall(I2S_NUM_0);
+  periph_module_reset(PERIPH_I2S0_MODULE);
+
+  delay(100);         // Give that poor microphone some time to setup.
   if (dmEnabled == 1) {
-
     Serial.println("Attempting to configure digital Microphone.");
-    delay(100);                                 // Give that poor microphone some time to setup.
-    
-#ifdef USE_ES7243
-    es7243_i2c_init_adc();
-#endif  // USE_ES7243
-
-    // Attempt to configure INMP441 Microphone
-    esp_err_t err;
-    const i2s_config_t i2s_config = {
-      .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),  // Receive, not transfer
-#ifdef USE_ES7243
-      .sample_rate = SAMPLE_RATE*4,                       // 11025 * 4 (44100) Hz
-#else
-      .sample_rate = SAMPLE_RATE*2,                       // 10240 * 2 (20480) Hz
-#endif // USE_ES7243
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,       // could only get it to work with 32bits
-#ifdef USE_ES7243
-      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-#else
-      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,        // LEFT when pin is tied to ground.
-#endif // USE_ES7243
-      .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,           // Interrupt level 1
-      .dma_buf_count = 8,                                 // number of buffers
-      .dma_buf_len = BLOCK_SIZE                           // samples per buffer
-    };
-    const i2s_pin_config_t pin_config = {
-      .bck_io_num = i2sckPin,     // BCLK aka SCK
-      .ws_io_num = i2swsPin,      // LRCL aka WS
-      .data_out_num = -1,         // not used (only for speakers)
-      .data_in_num = i2ssdPin     // DOUT aka SD
-    };
-    // Configuring the I2S driver and pins.
-    // This function must be called before any I2S driver read/write operations.
-    err = i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
-    if (err != ESP_OK) {
-      Serial.printf("Failed installing driver: %d\n", err);
-      while (true);
-    }
-    err = i2s_set_pin(I2S_PORT, &pin_config);
-    if (err != ESP_OK) {
-      Serial.printf("Failed setting pin: %d\n", err);
-      while (true);
-    }
-    Serial.println("I2S driver installed.");
-    delay(250);
-    // Test to see if we have a digital microphone installed or not.
-    float mean = 0.0;
-    int32_t samples[BLOCK_SIZE];
-    // TODO: I2S_READ_BYTES DEPRECATED, FIND ALTERNATE SOLUTION
-      size_t num_bytes_read = 0;
-
-      i2s_read(I2S_PORT, &samples, BLOCK_SIZE, &num_bytes_read, portMAX_DELAY); //esp_err_t result = 
-
-    /*int num_bytes_read = i2s_read_bytes(I2S_PORT,
-                                        (char *)samples,
-                                        BLOCK_SIZE,     // the doc says bytes, but its elements.
-                                        portMAX_DELAY); // no timeout
-    */
-
-    int samples_read = num_bytes_read / 8;
-    if (samples_read > 0) {
-      for (int i = 0; i < samples_read; ++i) {
-        mean += samples[i];
-      }
-      mean = mean/BLOCK_SIZE/16384;
-      if (mean != 0.0) {
-        Serial.println("Digital microphone is present.");
-      } else {
-        Serial.println("Digital microphone is NOT present.");
-    //    analogReadResolution(10);          // Default is 12, which is less linear. We're also only using 10 bits as a result of our ESP8266 history.
-      }
-    }
+    #ifdef USE_ES7243
+      audioSource = new ES7243(SAMPLE_RATE, BLOCK_SIZE, 16, 0xFFFFFFFF);
+    #else
+      audioSource = new I2SSource(SAMPLE_RATE, BLOCK_SIZE, 16, 0xFFFFFFFF);
+    #endif
   } else {
-    Serial.println("Using Analog Microphone.");
+      Serial.println("Attempting to configure analog Microphone.");
+      audioSource = new I2SAdcSource(SAMPLE_RATE, BLOCK_SIZE, 16, 0xFFF);
   }
+  delay(100);
+  audioSource->initialize();
+  delay(250);
 
-pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-sampling_period_us = round(1000000*(1.0/SAMPLE_RATE));
+  sampling_period_us = round(1000000*(1.0/SAMPLE_RATE));
 
-// Define the FFT Task and lock it to core 0
-xTaskCreatePinnedToCore(
-      FFTcode,                          // Function to implement the task
-      "FFT",                            // Name of the task
-      10000,                            // Stack size in words
-      NULL,                             // Task input parameter
-      1,                                // Priority of the task
-      &FFT_Task,                        // Task handle
-      0);                               // Core where the task should run
+  // Define the FFT Task and lock it to core 0
+  xTaskCreatePinnedToCore(
+        FFTcode,                          // Function to implement the task
+        "FFT",                            // Name of the task
+        10000,                            // Stack size in words
+        NULL,                             // Task input parameter
+        1,                                // Priority of the task
+        &FFT_Task,                        // Task handle
+        0);                               // Core where the task should run
 }
 
 // This gets called every time WiFi is (re-)connected. Initialize own network interfaces here
